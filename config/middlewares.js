a285e5b9e85498afa9556530cc4bf0d5a2ed14b8
Dlong <joselargocha@gmail.com>
2024-05-10 16:48:50 -0500

    midelware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,13 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
   {
     name: 'strapi::security',
     config: {
@@ -21,11 +28,4 @@ module.exports = [
       },
     },
   },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
 ];
