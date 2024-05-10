87c0093ef8a9eda9c71a47b971cad7013ba0936a
Dlong <joselargocha@gmail.com>
2024-05-10 16:02:40 -0500

    clody2

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,25 +1,12 @@
-module.exports = {
-  load: {
-    order: [
-      "strapi::logger",
-      "strapi::errors",
-      "strapi::security",
-      "strapi::cors",
-      "strapi::poweredBy",
-      "strapi::query",
-      "strapi::body",
-      "strapi::session",
-      "strapi::favicon",
-      "strapi::public",
-    ],
-  },
-  settings: {
-    cors: {
-      origin: ["https://tceimports.com/"],
-    },
-    public: {
-      path: "./public",
-      maxAge: 60000,
-    },
-  },
-};
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
