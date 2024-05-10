85553f01db3c465f7e918de0172087421c949b6d
Dlong <joselargocha@gmail.com>
2024-05-10 15:56:45 -0500

    setings images postg

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,25 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = {
+  load: {
+    order: [
+      "strapi::logger",
+      "strapi::errors",
+      "strapi::security",
+      "strapi::cors",
+      "strapi::poweredBy",
+      "strapi::query",
+      "strapi::body",
+      "strapi::session",
+      "strapi::favicon",
+      "strapi::public",
+    ],
+  },
+  settings: {
+    cors: {
+      origin: ["https://tceimports.com/"],
+    },
+    public: {
+      path: "./public",
+      maxAge: 60000,
+    },
+  },
+};
